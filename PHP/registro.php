f9e54bb527d9945ef1192fc01b44deb081b0c58f
Brandon <brandonde2002@gmail.com>
2024-03-14 11:03:12 -0600

    ya sirve

--- PHP/registro.php
+++ PHP/registro.php
@@ -11,10 +11,10 @@
         <div class="row justify-content-center">
             <div class="col-6">
                 <h2 class="text-center">Registro</h2>
-                <form action="postRegistro.php" method="post">
+                <form id="registroForm" action="postRegistro.php" method="post">
                     <div class="mb-3">
-                        <label for="username" class="form-label">Usuario:</label>
-                        <input type="text" class="form-control" id="username" name="username" required>
+                        <label for="usuario" class="form-label">Usuario:</label>
+                        <input type="text" class="form-control" id="usuario" name="username" required>
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Contraseña:</label>
@@ -30,7 +30,6 @@
                         <select class="form-select" id="rol" name="rol" required>
                             <option value="alumno">Alumno</option>
                             <option value="admin">Admin</option>
-                            
                         </select>
                     </div>
                     <button type="submit" class="btn btn-primary">Registrarse</button>
@@ -39,5 +38,22 @@
             </div>
         </div>
     </div>
+
+    <script>
+    // Validación para evitar palabras groseras en el nombre de usuario
+    document.getElementById('registroForm').addEventListener('submit', function(event) {
+        var palabrasGroseras = ['pene', 'puto', 'pendejo', 'imbecil','ano','joel']; // Array de palabras groseras
+        var nombre = document.getElementById('usuario').value.toLowerCase(); // Cambiado a 'usuario' para que coincida con el ID del campo de usuario
+        var esGrosera = palabrasGroseras.some(function(palabra) {
+            return nombre.includes(palabra); // Utilizamos some() para comprobar si alguna palabra grosera está presente
+        });
+        
+        if (esGrosera) {
+            alert('Por favor, no utilices lenguaje ofensivo en el formulario.');
+            event.preventDefault(); // Evitar el envío del formulario si se encuentran palabras groseras
+        }
+    });
+</script>
+
 </body>
 </html>
