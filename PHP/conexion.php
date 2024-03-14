54e43d15eff3ba5cf84bb784f203d474d32c064f
Brandon <brandonde2002@gmail.com>
2024-03-14 07:41:12 -0600

    deployarhuh

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,14 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "fenec";
-
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    // Establecer el modo de error de PDO a excepción
+    $conn = new PDO("sqlsrv:server = tcp:brayan-server2.database.windows.net,1433; Database = brayan-database", "brayan-server2-admin", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
 }
-?>
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
